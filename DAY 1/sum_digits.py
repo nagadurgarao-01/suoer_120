8af2498fdd70257d1e3a81bf94519d0d2a00cbe6
Kothem Naga Durga Rao <123251400+nagadurgarao-01@users.noreply.github.com>
2025-08-18 21:05:32 +0530

    Update sum_digits.py

--- DAY 1/sum_digits.py
+++ DAY 1/sum_digits.py
@@ -10,3 +10,18 @@ print(s)
 
 # 23232
 #12
+#logic 2
+
+n = int(input())
+sum = 0
+while n > 9:
+    n = str(n)
+    for i in n:
+        sum += int(i)
+    n = sum
+print(sum)
+
+#output
+
+# 23232
+#12
