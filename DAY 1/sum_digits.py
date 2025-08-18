3606ee85298c7e3235ddb89d546a7a4f4b1a9a2a
Kothem Naga Durga Rao <123251400+nagadurgarao-01@users.noreply.github.com>
2025-08-18 21:41:11 +0530

    Update sum_digits.py

--- DAY 1/sum_digits.py
+++ DAY 1/sum_digits.py
@@ -1,17 +1,4 @@
-n = int(input())
-s = 0
-while n!=0:
-    a = n%10
-    s += a
-    n = n//10
-print(s)
-
-#output
-
-# 23232
-#12
-#logic 2
-
+#sum of digits to single Digit
 n = int(input())
 sum = 0
 while n > 9:
